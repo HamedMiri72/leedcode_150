c30066ffe5919454d8a55a95e3102e02ffc3f0de
Deborah Burke <deborahburke@Deborahs-MacBook-Air.local>
2025-05-29 23:02:37 +0100

    merged sorted arrays

--- src/string/mergedsortedarray/Main.java
+++ src/string/mergedsortedarray/Main.java
@@ -1,7 +1,21 @@
 package string.mergedsortedarray;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+
+        int[] list1 = {1,2,3,0,0,0};
+        int[] list2= {6,8,9};
+        int m = 3;
+        int n = 3;
+
+
+        MergedSortedArray merger = new MergedSortedArray();
+        merger.merge(list1, m, list2, n);
+
+        System.out.println("Merged Array" + Arrays.toString(list1));
 
     }
 }
